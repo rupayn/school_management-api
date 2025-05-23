9c352c7d768504e04042a6687e53fdd5f9889ddf
Rupayan Nandi <rupayannandi8055@gmail.com>
2025-05-24 04:38:13 +0530

    setup

---
+++ .eslintrc.ts
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "consistent-return": "error"
+    }
+};
